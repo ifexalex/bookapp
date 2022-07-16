fab9a7004ef6a8e670dc348a2e3191e08ad6b3cd
Ifesinachi Ugwuoke <ifexalex2@gmail.com>
2022-07-16 03:58:53 +0100

    refactored code in author model and book model to include help_text and returned a more readable __str__ method for the both models

--- author/models.py
+++ author/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class Author(models.Model):
-    first_name = models.TextField()
-    last_name = models.TextField()
+    first_name = models.TextField(help_text=_('The first name of the author'),)
+    last_name = models.TextField(help_text=_('The last name of the author'),)
 
     def __str__(self):
-        return self.first_name
+        return f"{self.first_name} {self.last_name}"
--- book/models.py
+++ book/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from author.models import Author
+from django.utils.translation import ugettext_lazy as _
 
 class Book(models.Model):
-    name = models.TextField()
-    isbn = models.TextField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    name = models.TextField( help_text=_('The title of the Book'),)
+    isbn = models.TextField( help_text=_('The ISBN of the Book'),)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, help_text=_('The author of the Book; this is a foreignkey to the author table'),)
     
     def __str__(self):
-        return self.name
+        return f"{self.name} - {self.author}"
