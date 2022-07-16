1f34f6ea84e152f7b379902f1b019d356d6cb1e8
Ifesinachi Ugwuoke <ifexalex2@gmail.com>
2022-07-16 03:48:42 +0100

    added permission classes attribute to the Viewsets to authenticate any user

--- author/views.py
+++ author/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import AuthorSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Author
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
     serializer_class = AuthorSerializer
     http_method_names = ['get', 'post', 'put']
+    permission_classes =(AllowAny,)
 
     def get_queryset(self):
         return Author.objects.all()
--- book/views.py
+++ book/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from.serializers import BookSerializer
 from.models import Book
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
 class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     http_method_names = ['get', 'post', 'put']
+    permission_classes = (AllowAny,)
 
     def get_queryset(self):
         return Book.objects.all()
