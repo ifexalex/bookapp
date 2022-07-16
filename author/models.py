ca53cf3b970965ef53f3b35a1a59720cc28347bf
Ifesinachi Ugwuoke <ifexalex2@gmail.com>
2022-07-16 02:29:00 +0100

    created author model, registered author model  in admin.py and included  author baseUrl to the project urls.py

--- author/models.py
+++ author/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.TextField(max_length=50)
+    last_name = models.TextField(max_length=50)
+
+    def __str__(self):
+        return self.first_name
