6a3454daa0441503b24b7c422f8077c2d9ce0461
Ifesinachi Ugwuoke <ifexalex2@gmail.com>
2022-07-16 03:36:02 +0100

    added get_queryset function to return serialized book data and serialized author data

--- author/views.py
+++ author/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import AuthorSerializer
+from .models import Author
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
     serializer_class = AuthorSerializer
     http_method_names = ['get', 'post', 'put']
+
+    def get_queryset(self):
+        return Author.objects.all()
--- book/views.py
+++ book/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from.serializers import BookSerializer
+from.models import Book
 
 
 class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     http_method_names = ['get', 'post', 'put']
+
+    def get_queryset(self):
+        return Book.objects.all()
